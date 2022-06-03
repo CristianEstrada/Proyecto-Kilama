f2593ae9135163cb0899625ce3c16aa71cc4ef69
Cristian Rodrigo Estrada Romero <ecristian963@gmail.com>
2022-06-03 16:20:30 -0500

    Update App.js

--- kilama/App.js
+++ kilama/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { t } from 'react-native-tailwindcss';
-import styled from 'styled-components/native';
 
 import StackNavigator from "./StackNavigator";
 import { NavigationContainer } from "@react-navigation/native";
